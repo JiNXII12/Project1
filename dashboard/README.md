990e7d8572a12d64f50c88868ce6286c53139a12
JiNXII12 <98947161+JiNXII12@users.noreply.github.com>
2024-02-13 01:09:40 +0700

    feat

---
+++ dashboard/README.md
@@ -0,0 +1,10 @@
+# Tailwind & Vue.js admin dashboard template
+
+![Cedit pasqualevitiello ](https://github.com/cruip/vuejs-admin-dashboard-template)
+
+# Edit Dashboard 
+
+![alt text](image.png)
+
+# Dashboard Water quality sensor
+( - temp - pH )
---
+++ dashboard/src/pages/Dashboard.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="flex h-screen overflow-hidden">
+
+    <!-- Sidebar -->
+    <Sidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
+
+    <!-- Content area -->
+    <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      
+      <!-- Site header -->
+      <Header :sidebarOpen="sidebarOpen" @toggle-sidebar="sidebarOpen = !sidebarOpen" />
+
+      <main>
+        <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+
+
+          
+          <!-- Dashboard actions -->
+          <div class="sm:flex sm:justify-between sm:items-center mb-8">
+
+            <!-- Left: Avatars -->
+
+
+          </div>
+
+          <!-- Cards -->
+          <div class="grid grid-cols-12 gap-6">
+
+            <!-- Line chart (Acme Plus) -->
+            <DashboardCard01 />
+            <!-- Line chart (Acme Advanced) -->
+            <DashboardCard02 />
+            <!-- Line chart (Acme Professional) -->
+            <DashboardCard03 />
+            <!-- Bar chart (Direct vs Indirect) -->
+            <DashboardCard04 />
+            <!-- Line chart (Real Time Value) -->
+            <DashboardCard05 />
+            <!-- Doughnut chart (Average) -->
+            <DashboardCard06 />
+            <!-- Line chart (Sales Over Time) -->
+            <DashboardCard08 />
+
+
+          </div>
+
+        </div>
+      </main>
+
+      <Banner />
+
+    </div> 
+
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Sidebar from '../partials/Sidebar.vue'
+import Header from '../partials/Header.vue'
+import FilterButton from '../components/DropdownFilter.vue'
+import Datepicker from '../components/Datepicker.vue'
+import DashboardCard01 from '../partials/dashboard/DashboardCard01.vue'
+import DashboardCard02 from '../partials/dashboard/DashboardCard02.vue'
+import DashboardCard03 from '../partials/dashboard/DashboardCard03.vue'
+import DashboardCard04 from '../partials/dashboard/DashboardCard04.vue'
+import DashboardCard05 from '../partials/dashboard/DashboardCard05.vue'
+import DashboardCard06 from '../partials/dashboard/DashboardCard06.vue'
+import DashboardCard08 from '../partials/dashboard/DashboardCard08.vue'
+
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Sidebar,
+    Header,
+    FilterButton,
+    Datepicker,
+    DashboardCard01,
+    DashboardCard02,
+    DashboardCard03,
+    DashboardCard04,
+    DashboardCard05,
+    DashboardCard06,
+    DashboardCard08,
+
+  },
+  setup() {
+
+    const sidebarOpen = ref(false)
+
+    return {
+      sidebarOpen,
+    }  
+  }
+}
+</script>
